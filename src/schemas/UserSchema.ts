28686710c6acf8310cea2f3867b6b9a8698cfe5a
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2026-02-10 14:01:13 +0100

    refactor: clean comments and replace magic numbers

--- src/schemas/UserSchema.ts
+++ src/schemas/UserSchema.ts
@@ -1,15 +1,12 @@
-// IDEA: Regroup FormFieldsSchema and FormFields from here and "RegisterUserForm" into one file ?
-
 import z from 'zod';
 
-// TODO: Replace the magic number by CONSTANT like MIN_PASSWORD_LENGTH.
-//const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-//const MIN_PASSWORD_LENGTH = 12;
-//const MAX_PASSWORD_LENGTH = 100;
+const MIN_PASSWORD = 12;
+const MAX_PASSWORD = 100;
 
+// IDEA: Maybe rewrite the error message to be clearer for users ?
 export const UserSchema = z.object({
     email: z.email(),
-    password: z.string().min(12).max(100),
+    password: z.string().min(MIN_PASSWORD).max(MAX_PASSWORD),
 });
 
 export type UserFields = z.infer<typeof UserSchema>;
