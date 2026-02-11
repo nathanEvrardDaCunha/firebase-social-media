47a489ff298e27d7acf6b6eacf8794cf1caf8914
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2026-02-11 16:15:54 +0100

    feat: create feed page to display publications

---
+++ TASK-OF-DAY.md
@@ -0,0 +1,7 @@
+### 11/02/2026
+
+- [x] Allow every user to read publications.
+- [ ] Allow authentificated user to write publications. (did not test with non authenticated user yet)
+- [ ] Allow authentificated user to delete their own publications. (not implemented yet)
+
+- Change label of form
--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -1,7 +1,74 @@
+// Any user can read publications
+// Only authenticated user can write publications
+// Only authenticated user can delete their, and only their, own publications
+// No user should be able to modify his publications
+// TODO: Will need to limit the number of publication to not load 5000+ one at the same time.
+
+import { collection, getDocs } from 'firebase/firestore';
+import WritePublicationForm from '../components/WritePublicationForm';
+import { auth, db } from '../config/firebase-config';
+import { useEffect, useState } from 'react';
+import {
+    PublicationSchema,
+    type PublicationFields,
+} from '../schemas/PublicationSchema';
+
 const FeedPage = () => {
+    const [publications, setPublications] = useState<PublicationFields[]>([]);
+
+    const publicationsCollectionRef = collection(db, 'publications');
+
+    // Extract this to "ReadPublications"
+    useEffect(() => {
+        const getPublications = async () => {
+            try {
+                const data = await getDocs(publicationsCollectionRef);
+
+                const filteredData = data.docs.map((doc) => {
+                    const publication = PublicationSchema.parse({
+                        id: doc.id,
+                        userId: doc.data().userId,
+                        message: doc.data().message,
+                        creationDate: doc.data().creationDate,
+                    });
+
+                    return publication;
+                });
+
+                console.log(filteredData);
+
+                setPublications(filteredData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getPublications();
+    }, []);
+
     return (
         <section>
             <h1>Feed page</h1>
+
+            {/* IDEA: Export the publications display to it's own module */}
+            {publications.length > 0 ? (
+                publications.map((publication) => (
+                    <section key={publication.id}>
+                        <h4>{publication.userId}</h4>
+
+                        <time datatype={publication.creationDate.toUTCString()}>
+                            {publication.creationDate.toUTCString()}
+                        </time>
+
+                        <p>{publication.message}</p>
+                    </section>
+                ))
+            ) : (
+                <h1>No publications yet.</h1>
+            )}
+
+            {/* TODO: CHeck if it really make sure the currentUser is defined for the form to appear */}
+            {auth.currentUser ? <WritePublicationForm /> : null}
         </section>
     );
 };
